5960db7171f6a1e552c4ff17fa32f688dddeb331
becaye10 <becaye.kadam@icloud.com>
2026-09-17 13:41:33 +0200

    första

---
+++ berättelse.md
@@ -0,0 +1,3 @@
+Ett rymdskepp har krashat och det första valet är att antingen stanna eller gå ut ur skeppet 
+om man väljer att gå ut så märker man att det inte går att andas utan en mask så man måste gå in i skeppet och hämta den
+om man väljer att kolla runt så ser man masken och tar med den för säkerhets skull men man kommer också på att det finns en liten rymdgokart som man kan ta
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# Här skriver du ditt textäventyr
+# Här skriver du ditt 
+import time
+SpelarensNamn = input("vad vill du bli kallad? ")
+time.sleep(1)
+print("hej " + SpelarensNamn + " välkommen till ditt textäventyr")
+time.sleep(1)
+print("Du har precis krashat på en okänd planet och ditt skepp är paj!")
+GåUt = input("Vill du gå ut eller stanna i skeppet och kolla runt? ")
+if GåUt == "gå ut":
+    print("Du märker att det inte går att andas och tvingas gå in i skeppet och hämta din mask ändå")
